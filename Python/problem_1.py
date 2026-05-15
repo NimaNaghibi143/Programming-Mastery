3115f2bc0d0f783be50cc804c20f3661956a3944
Mohammad Homanloo <homanloom@gmail.com>
2026-05-15 14:28:35 +0330

    Implement initial solution

--- Python/problem_1.py
+++ Python/problem_1.py
@@ -29,16 +29,32 @@
 # names llive ? Global vars, classes, func param?!
 
 BASE_PRICE = 5.0                        # Dollars
-ATTENDEES_PER_DOLLAR = 150             # 150 people per dollar (15 people per 10 cents)
+ATTENDEES_PER_DOLLAR = 150              # 150 people per dollar (15 people per 10 cents)
 BASE_ATTENDEES = 120                    # Numbers of attendees at base price
 FIXED_COST = 180.0                      # Dollars
 COST_PER_ATTENDEE = 0.04                # Dollars
 
-def compute_attendees(price):
-    return BASE_ATTENDEES - (price - BASE_PRICE)*ATTENDEES_PER_DOLLAR
+def compute_attendees(TP):
+    return int(BASE_ATTENDEES + (BASE_PRICE-TP)*ATTENDEES_PER_DOLLAR)
 
-def compute_cost(attendees):
-    return FIXED_COST + COST_PER_ATTENDEE*attendees
+def compute_revenue(TP):
+    return float(TP*compute_attendees(TP))
 
-def compute_profit(price):
-    ...
+def compute_cost(TP):
+    return float(FIXED_COST + COST_PER_ATTENDEE*compute_attendees(TP))
+
+def compute_profit(TP):
+    return float(compute_revenue(TP) - compute_cost(TP))
+
+def optimize(LB, UB):
+    profit = []
+    # The fact that "range" does not accept floats as steps is painful,
+    # while numpy "arrange" does so with ease...
+    for TP in range(LB*10, UB*10):
+        TP = TP/10
+        profit.append((TP, compute_profit(TP)))
+    profit.sort(key=lambda x: x[1], reverse=True)
+    best_TP, best_profit = profit[0]
+    return print(f"Best Ticket Price is {best_TP} with a corresponding value of ${best_profit} in Profit.")
+
+optimize(0, 100)

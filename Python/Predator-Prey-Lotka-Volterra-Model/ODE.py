4cb01c641767f071fd6351f0aa6a51a98155e74c
Mohammad Homanloo <homanloom@gmail.com>
2026-05-18 00:16:14 +0330

    Change staticmethod approach and add helper function for initialization of parameters

--- Python/Predator-Prey-Lotka-Volterra-Model/ODE.py
+++ Python/Predator-Prey-Lotka-Volterra-Model/ODE.py
@@ -3,17 +3,26 @@ import numpy as np
 
 class ODESolver:
 
-    @staticmethod
-    def Euler(model, initial_state, h, N):
-        t = 0
-        state = np.zeros((N, len(initial_state)))
-        t_values = np.zeros(N)
-        state[0] = initial_state
-        for i in range(0, N-1):
-            derivatives = model.rhs(state[i])
-            state[i+1] = state[i] + h*(np.array(derivatives))
-            t += h
-            t_values[i+1] = t
+    def __init__(self, model, initial_state, h, N):
+        self.model = model
+        self.initial_state = initial_state
+        self.h = h
+        self.N = N
 
+    def _initialize(self):
+        """Helper function to initialize parameters and avoid repeated code"""
+        self.t = 0
+        self.state = np.zeros((self.N, len(self.initial_state)))
+        self.t_values = np.zeros(self.N)
+        self.state[0] = self.initial_state
 
-        return np.round(state, 1), t_values
+    def Euler(self):
+        self._initialize()
+
+        for i in range(self.N-1):
+            derivatives = self.model.rhs(self.state[i])
+            self.state[i+1] = self.state[i] + self.h*(np.array(derivatives))
+            self.t += self.h
+            self.t_values[i+1] = self.t
+
+        return np.round(self.state, 1), self.t_values

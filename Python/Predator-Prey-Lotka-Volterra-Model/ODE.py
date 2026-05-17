433abd202fcdcff8bfeaff8f1ff05fe3e96b4be1
Mohammad Homanloo <homanloom@gmail.com>
2026-05-17 23:49:02 +0330

    Change ODE methods implementation from functional to object-oriented

--- Python/Predator-Prey-Lotka-Volterra-Model/ODE.py
+++ Python/Predator-Prey-Lotka-Volterra-Model/ODE.py
@@ -1,16 +1,19 @@
 import numpy as np
 
 
-def Euler(model, initial_state, h, N):
-    t = 0
-    state = np.zeros((N, len(initial_state)))
-    t_values = np.zeros(N)
-    state[0] = initial_state
-    for i in range(0, N-1):
-        derivatives = model.rhs(state[i])
-        state[i+1] = state[i] + h*(np.array(derivatives))
-        t += h
-        t_values[i+1] = t
+class ODESolver:
+
+    @staticmethod
+    def Euler(model, initial_state, h, N):
+        t = 0
+        state = np.zeros((N, len(initial_state)))
+        t_values = np.zeros(N)
+        state[0] = initial_state
+        for i in range(0, N-1):
+            derivatives = model.rhs(state[i])
+            state[i+1] = state[i] + h*(np.array(derivatives))
+            t += h
+            t_values[i+1] = t
 
 
-    return np.round(state, 1), t_values
+        return np.round(state, 1), t_values

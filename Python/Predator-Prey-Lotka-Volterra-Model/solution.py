c7f6824ab5a132088dfa0c5d5b3eaff817dde440
Mohammad Homanloo <homanloom@gmail.com>
2026-05-18 00:16:47 +0330

    Update solution to handle the new object-oriented version

--- Python/Predator-Prey-Lotka-Volterra-Model/solution.py
+++ Python/Predator-Prey-Lotka-Volterra-Model/solution.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 from LotkaVolterra import LotkaVolterra
-from ODE import Euler
+from ODE import ODESolver
 from plot import plot
 
 
 lv_model = LotkaVolterra()
-states, t_values = Euler(lv_model, [10, 10], 0.001, 50000)
+solver = ODESolver(lv_model, [10, 10], 0.01, 10000)
+states, t_values = solver.Euler()
 
 fig = plot(states, t_values)
 plt.show()

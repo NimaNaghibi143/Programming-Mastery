9fbcd0f6c473eb61ef1374b4ad973d5a3d433738
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-05-19 18:32:08 +0330

    feat: finish def of DSs and initialize the root node

--- C/binary-tree/tree.c
+++ C/binary-tree/tree.c
@@ -1,7 +1,16 @@
 /* tree.c */
 #include "tree.h"
 
+Tree root = { .n = {
+    .tag = (TagRoot | TagNode), /* the root is a node */
+    .north = (Node *)&root,
+    .west = 0,
+    .east = 0,
+    .path = "/"
+}
+};
 
 int main() {
-    printf("Hello world\n");
+
+    return 0;
 }

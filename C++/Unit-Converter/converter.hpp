0457ce2e0ad91de63ff04693a3baab4692867b0b
Mohammad Homanloo <homanloom@gmail.com>
2026-06-02 14:20:11 +0330

    Add unit classes with toString functions

--- C++/Unit-Converter/converter.hpp
+++ C++/Unit-Converter/converter.hpp
@@ -3,6 +3,66 @@
 
 #include <iostream>
 
+// Unit classes with tool functions
+struct PressureUnit 
+{
+    const int Pa    = 1;
+    const int Bar   = 2;
+    const int Atm   = 3;
+    const int Psi   = 4;
+
+    static std::string toString(int unit)
+    {
+        switch (unit)
+        {
+        case 1:     return "Pa";
+        case 2:     return "Bar";
+        case 3:     return "Atm";
+        case 4:     return "Psi";
+        default:    return "N/A";
+        }
+    }
+};
+
+struct TemperatureUnit 
+{
+    const int K     = 1;
+    const int C     = 2;
+    const int F     = 3;
+    const int R     = 4;
+
+    static std::string toString(int unit)
+    {
+        switch (unit)
+        {
+        case 1:     return "K";
+        case 2:     return "C";
+        case 3:     return "F";
+        case 4:     return "R";
+        default:    return "N/A";
+        }
+    }
+};
+
+struct EnergyUnit 
+{
+    const int J     = 1;
+    const int Cal   = 2;
+    const int Btu   = 3;
+
+    static std::string toString(int unit)
+    {
+        switch (unit)
+        {
+        case 1:     return "J";
+        case 2:     return "Cal";
+        case 3:     return "Btu";
+        default:    return "N/A";
+        }
+    }
+};
+
+// Result class for history loging
 struct ConversionResults
 {
     double inputValue;

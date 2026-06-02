db145b9d5a1f942e749f94c818a1fe1c74d908cb
Mohammad Homanloo <homanloom@gmail.com>
2026-06-02 14:22:41 +0330

    Implement unit and value input gathering

--- C++/Unit-Converter/main.cpp
+++ C++/Unit-Converter/main.cpp
@@ -4,7 +4,11 @@
 
 int main()
 {
-    int category {1};
+    int category    {1};
+    int fromUnit    {1};
+    int toUnit      {2};
+    double value    {0};
+
     std::cout << "\n==== Welcome to the Unit Converter CLI tool! ====\n\n";
     std::cout << "Please select the convertion category:\n";
     std::cout << "1. Pressure\n";
@@ -18,16 +22,63 @@ int main()
     if (category == 1)
     {
         // Pressure convertion
+        std::cout << "\nAvailable Pressure Units:\n";
+        std::cout << "1. Pa\n";
+        std::cout << "2. Bar\n";
+        std::cout << "3. Atm\n";
+        std::cout << "4. Psi\n";
+
+        std::cout << "From unit: ";
+        std::cin >> fromUnit;
+        std::cout << "From unit: ";
+        std::cin >> toUnit;
+        std::cout << "Value: ";
+        std::cin >> value;
+
+        std::cout   << "\nConverting value " << value 
+                    << " from " << PressureUnit::toString(fromUnit)
+                    << " to " << PressureUnit::toString(toUnit);
     }
 
     else if (category == 2)
     {
         // Temperature convertion
+        std::cout << "\nAvailable Temperature Units:\n";
+        std::cout << "1. K\n";
+        std::cout << "2. C\n";
+        std::cout << "3. F\n";
+        std::cout << "4. R\n";
+
+        std::cout << "From unit: ";
+        std::cin >> fromUnit;
+        std::cout << "From unit: ";
+        std::cin >> toUnit;
+        std::cout << "Value: ";
+        std::cin >> value;
+
+        std::cout   << "\nConverting value " << value 
+                    << " from " << TemperatureUnit::toString(fromUnit)
+                    << " to " << TemperatureUnit::toString(toUnit);
     }
 
     else if (category == 3)
     {
         // Energy convertion
+        std::cout << "\nAvailable Energy Units:\n";
+        std::cout << "1. J\n";
+        std::cout << "2. Cal\n";
+        std::cout << "3. Btu\n";
+
+        std::cout << "From unit: ";
+        std::cin >> fromUnit;
+        std::cout << "From unit: ";
+        std::cin >> toUnit;
+        std::cout << "Value: ";
+        std::cin >> value;
+
+        std::cout   << "\nConverting value " << value 
+                    << " from " << EnergyUnit::toString(fromUnit)
+                    << " to " << EnergyUnit::toString(toUnit);
     }
 
     else

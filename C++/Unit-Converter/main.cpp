3236426f5ed9d773520f619fbc2e7a494893108d
Mohammad Homanloo <homanloom@gmail.com>
2026-06-02 13:22:31 +0330

    Implement first menu options input

--- C++/Unit-Converter/main.cpp
+++ C++/Unit-Converter/main.cpp
@@ -1,7 +1,18 @@
 #include "converter.hpp"
 #include <iostream>
 
+
 int main()
 {
+    int category {1};
+    std::cout << "\n==== Welcome to the Unit Converter CLI tool! ====\n\n";
+    std::cout << "Please select the convertion category:\n";
+    std::cout << "1. Pressure\n";
+    std::cout << "2. Temperature\n";
+    std::cout << "3. Energy\n";
+
+    std::cout << "Choice number: ";
+    std::cin  >> category;
+
     return 0;
 }

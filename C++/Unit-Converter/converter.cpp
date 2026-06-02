db2af2b967ed9d771c57de11e171f0c581981a29
Mohammad Homanloo <homanloom@gmail.com>
2026-06-02 17:16:03 +0330

    Add temperature and energy convertsions

--- C++/Unit-Converter/converter.cpp
+++ C++/Unit-Converter/converter.cpp
@@ -37,21 +37,59 @@ double convertPressure(
 
 double convertTemperature(
     double value,
-    const std::string& from,
-    const std::string& to
+    const std::string& fromUnit,
+    const std::string& toUnit
 )
 {
-    // Implement temperature conversion
-    return 0;
+    double kelvin {1};
+
+    if (fromUnit == "K")
+        kelvin = value;
+    else if (fromUnit == "C")
+        kelvin = value + 273.15;
+    else if (fromUnit == "F")
+        kelvin = ((value - 32) / 1.8) + 273.15;
+    else if (fromUnit == "R")
+        kelvin = value / 1.8;
+    else
+        kelvin = value;
+
+    if (toUnit == "K")
+        return kelvin;
+    else if (toUnit == "C")
+        return kelvin - 273.15;
+    else if (toUnit == "F")
+        return ((kelvin - 273.15) * 1.8) + 32;
+    else if (toUnit == "R")
+        return kelvin * 1.8;
+    else
+        return kelvin;
 }
 
 
 double convertEnergy(
     double value,
-    const std::string& from,
-    const std::string& to
+    const std::string& fromUnit,
+    const std::string& toUnit
 )
 {
-    // Implement energy conversion
-    return 0;
+    double joule {1};
+
+    if (fromUnit == "J")
+        joule = value;
+    else if (fromUnit == "Cal")
+        joule = value * 4.184;
+    else if (fromUnit == "Btu")
+        joule = value * 1055.06;
+    else
+        joule = value;
+
+    if (toUnit == "J")
+        return joule;
+    else if (toUnit == "Cal")
+        return joule / 4.184;
+    else if (toUnit == "Btu")
+        return joule / 1055.06;
+    else
+        return joule;
 }

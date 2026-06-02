90d2b76e648c66023ec1885faf690ac71a30f376
Mohammad Homanloo <homanloom@gmail.com>
2026-06-02 12:30:09 +0330

    Change project structure and plans
    The previous design that I was aiming for was a bit to much for me at this stage. I will tackle an easier approach now, maybe expand it later.

---
+++ C++/Unit-Converter/converter.hpp
@@ -0,0 +1,12 @@
+//This will solve the duplicated declarations in header files
+#pragma once
+
+#include <iostream>
+
+struct ConversionResults
+{
+    double inputValue;
+    double outputValue;
+    std::string inputUnit;
+    std::string outputUnit;
+};
--- C++/Unit-Converter/main.cpp
+++ C++/Unit-Converter/main.cpp
@@ -1,9 +1,7 @@
-#include "units.hpp"
+#include "converter.hpp"
 #include <iostream>
 
 int main()
 {
-    std::cout << static_cast<int>(PressureUnits::Atm);
-
     return 0;
 }
